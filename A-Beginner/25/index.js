1ce04980cb16f6c54855bbb7b65924914f255770
Ebba900 <ebba900@gmail.com>
2020-10-18 17:58:40 +0200

    25

---
+++ A-Beginner/25/index.js
@@ -0,0 +1,12 @@
+function islagom(x){
+return 8 > x && x < 16
+} 
+
+function islagom2(y){
+    if (y < 8)  {return false}
+        else if (y > 16 )
+    {return false } 
+        else { return true
+
+        }
+    }
